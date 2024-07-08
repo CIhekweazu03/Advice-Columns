1864a533b4556802a74dabe042e0e2682ff8020d
CIhekweazu03 <90338067+CIhekweazu03@users.noreply.github.com>
2024-07-08 16:08:18 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+# Title of the article
+st.title("Freshmen 15: 15 Things I Would Do If I Were Starting All Over")
+
+# Content of the article
+content = """
+### 1. Get involved with Creative Inquiries (CIs)
+CIs are probably the single biggest cheat code you have available to you as a Clemson student. They serve as a very low barrier to entry into research that most students elsewhere wish they could get.
+
+### 2. Apply to internships and REUs as early as possible
+Most internships and some REUs are on rolling deadlines whether they directly state it or not. Meaning that the longer you wait the more difficult it will be for you to find a role. You have to view these opportunities like a funnel; at the start, companies and schools are looking far and wide for qualified candidates to fill their numerous roles, and near the end, only a few truly amazing candidates will be able to make it through. 
+
+### 3. Time management
+You’re going to be busy; there’s truly nothing you can do about it except for managing your time wisely. Get things done the moment you have an opportunity to get them done.
+
+### 4. Task management
+Immediately take note of whatever task you are supposed to complete and when you need to have that task completed. It can be incredibly easy to forget an assignment or an application deadline when you’re trying to remember everything you have to do rather than writing it down somewhere.
+
+### 5. Go to office hours often
+Even if you’re doing well in class, it never hurts to go to office hours to establish a relationship with a professor. You can talk about concepts from class, life anecdotes that they may have given during a lecture, or look into whatever research they’ve done up to that point and discuss that.
+
+### 6. Interact with the people sitting beside you in class
+Odds are many of the people in your introductory classes for your major will be there from the start to the finish. Establishing a relationship with some of those people is a straightforward way to have a network of people you can go to on a myriad of things, such as which electives to take, who to take them with, which professors to take/avoid, etc. 
+
+### 7. Say yes to opportunities
+This will probably be the opposite of what you will hear a lot of as a freshman. “It’s ok to say no,” and things of that sort are stalwart pieces of advice targeted at freshmen. And while I do agree with the general premises of that advice, it is equally important to just say yes and go where life takes you. I can promise you that you will not remember that weekend when you did nothing but scroll on social media or play video games. However, you will remember that time when you went paintballing in Anderson with a group of friends on incredibly short notice.
+
+### 8. Check your emails daily
+As a freshman, you are going to be bombarded with a lot of emails, and as difficult as it might be, check them daily. It doesn’t have to be a full read-through of each and every one of them, but at least give them all a quick glance to make sure you are not missing anything important.
+
+### 9. Study early and (somewhat) often
+Some (if not most) classes are going to move rapidly and play a giant game of Jenga with the concepts covered in class. Unfortunately, if you lose this game, your grade will be the thing to topple over, not your pride. If you find yourself lost or didn’t pay that much attention in class that day, please just review your notes, watch a YouTube video, or go to office hours to catch up. The last thing you want to do is wait until the last minute and try to cram for an exam worth a good chunk of your grade while trying to untangle the stream of content you missed.
+
+### 10. Get involved and stay consistent with clubs
+Clubs are going to be the easiest way to make new friends and connections with people because, presumably, you already have something in common with everyone there, and when it comes to making friends, that’s 90% of the battle.
+
+### 11. When it comes to your resume, stay ready so you don’t have to get ready
+Keep a nice, organized, ready-to-go copy of your resume at all times. You never know when an opportunity might present itself.
+
+### 12. Be reliable
+This goes for classes, friendships, research, anything; if you say you will do something, do it. Being reliable is one of the quickest ways to open up so many doors that you didn’t even know existed. On the flip side, being unreliable will be the fastest way to shut doors that were tailor-made for you to walk through.
+
+### 13. Find your escape
+Life will be stressful; find your escape and stick with it. It could be video games, hanging out with friends, exercising, etc. Finding something you can do to take your mind off the stresses and current ongoings of your life will help keep you sane in times of strife.
+
+### 14. Steal ideas from successful people 
+No, I don’t mean plagiarizing essays. Instead, just copy the lifestyle and tips from people who are where you want to be in life. If you’ve met a senior in your major doing all the things that you aspire to one day do, learn and take from them. There is absolutely no reason to reinvent the wheel, especially when most successful people have already taken the wheel and made modifications to it for their specific circumstances.
+
+### 15. You are going to fail, and that’s ok!
+Lastly, and quite frankly, most importantly, you are going to fail, a lot! And by failing, I don’t necessarily mean failing an exam, project, quiz, etc. (although sometimes it may happen). I mean that you will potentially mess up in any and all aspects of life. You may procrastinate working on a project until the last minute; you may forget to do the take-home quiz for that one class; you may forget, or you might miss a deadline for an opportunity you really cared about. But honestly, you are a freshman and making mistakes is expected. As a Breakthrough Scholar, a lot is expected of you, and rightfully so; your acceptance into this program is enough evidence that you are highly capable of meeting those expectations, but sometimes you are just going to fall short, and that’s ok. Ultimately, you are not defined by your mistakes but rather by how you react and recover from them.
+"""
+
+st.markdown(content)
